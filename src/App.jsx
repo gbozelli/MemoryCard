873ca4070e691d43fcd3cfe02c173bd3b7d36fd4
Bozelli <gabriel2004bozellidias@gmail.com>
2025-03-03 15:10:35 -0300

    loading screen added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Card from './Card.jsx';
 import './App.css';
 
@@ -8,6 +8,13 @@ const initialPokemons = [
   'diglett', 'meowth', 'psyduck', 'mankey', 'abra', 'bellsprout', 
   'geodude', 'voltorb',
 ];
+
+async function getData(pokemon) {
+  const response = await fetch(
+    `https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+  const data = await response.json();
+  return data;
+}
 
 function shuffle(array) {
   const shuffledArray = array; 
@@ -25,15 +32,11 @@ function shuffle(array) {
 }
 
 function App() {
-  const [pokemons, setPokemons] = useState([
-    'pikachu', 'bulbasaur', 'squirtle', 'charmander', 'pidgey', 'rattata', 
-    'snorlax', 'nidorina', 'vulpix', 'jigglypuff', 'zubat', 'oddish', 
-    'diglett', 'meowth', 'psyduck', 'mankey', 'abra', 'bellsprout', 
-    'geodude', 'voltorb',
-  ]);
+  const [pokemons, setPokemons] = useState([]);
   const [count, setCount] = useState(0);
   const [record, setRecord] = useState(0);
   const [allFalse, setAllFalse] = useState(false);
+  const [isLoading, setIsLoading] = useState(true); 
 
   const updateParent = (state) => {
     if (state === true) {
@@ -46,23 +49,44 @@ function App() {
         setRecord(count);
       }
       setCount(0); 
-      setPokemons(shuffle(initialPokemons));
+      setPokemons(shuffle(pokemons));
       setAllFalse(true);
     }
   };
 
+  useEffect(() => {
+    const fetchData = async () => {
+       const pokemonData = await Promise.all(
+        initialPokemons.map((pokemon) => getData(pokemon))
+      );
+      setPokemons(pokemonData);
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
-      <div className='header'>
+      
+      {isLoading ? (
+        <div className='loading'>
+          <div className='spinner'></div>
+          <p>Carregando cartas...</p>
+        </div>
+      ) : (<>
+        <div className='header'>
         <div>Pokemon Card Game</div>
         <div>Count: {count}</div>
         <div>Record: {record}</div>
       </div>
-      <div className='cards'>
-        {pokemons.map((pokemon) => (
-          <Card key={pokemon} pokemon={pokemon} updateParent={updateParent} allFalse={allFalse}/>
-        ))}
-      </div>
+        <div className='cards'>
+          {pokemons.map((pokemon, index) => (
+            <Card key={pokemon.name} pokemon={pokemon.name} img={pokemon.sprites.front_default} updateParent={updateParent} allFalse={allFalse} />
+          ))}
+        </div>
+        </>
+      )}
     </>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,64 +1,35 @@
-import { useState , useEffect } from 'react'
-import './Card.css'
+import { useState, useEffect } from 'react';
+import './Card.css';
 
 function capitalizeFirstLetter(val) {
-    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
-}
-
-async function getData(pokemon) {
-  const response = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-  const data = await response.json();
-  return data;
-}
-
-function getImg(data){
-  return data.sprites.front_default;
-}
-
-function getName(data){
-  return data.name;
+  return String(val).charAt(0).toUpperCase() + String(val).slice(1);
 }
 
 function Card(props) {
-  const [img, setImg] = useState();
-  const [name, setName] = useState();
   const [click, setClick] = useState(false);
-  const updateParent = props.updateParent;
-  
+
   useEffect(() => {
-  if (props.allFalse === true) {
-    setClick(false);
-  }
-}, [props.allFalse]);
+    if (props.allFalse === true) {
+      setClick(false); 
+    }
+  }, [props.allFalse]);
 
   const changeClick = () => {
-    if(click===false){
+    if (!click) {
       setClick(true);
-      updateParent(true);
-    }
-    if(click===true){
+      props.updateParent(true); 
+    } else {
       setClick(false);
-      updateParent(false);
+      props.updateParent(false);
     }
-  }
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getData(props.pokemon);
-      setImg(getImg(data));
-      setName(getName(data));
-    };
-
-    fetchData();
-  }, []);
+  };
 
   return (
     <div className='poke' onClick={changeClick}>
-      <img src={img}/>
-      <div>{capitalizeFirstLetter(name)}</div>
+      <img src={props.img} alt={props.pokemon} />
+      <div>{capitalizeFirstLetter(props.pokemon)}</div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
